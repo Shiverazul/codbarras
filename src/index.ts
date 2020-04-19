632ebe43bcabb019dd32dbce121f08f7808353ff
shiverazul <shiverazul@gmail.com>
2020-04-19 15:22:40 +0200

    borrar table de descipcionResultado

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,28 @@ let ddbb = null
 $.getJSON("A-HTML.json", function (JSON) {
     ddbb = JSON;
 });
+function aclaradoTabla() {
+//    (document.getElementById('table')).nodeValue = "";
+//    $("#table_of_items tr").remove();
+//    $("table id tr").remove()
+//var table = document.getElementById("table"); Table.innerHTML = ""; 
+//const table=document.createElement("table")
+//        document.getElementById("table").innerHTML = ""; 
 
+}
 function init() {
     document.getElementById("borrar").addEventListener("click", () => {
-        Borrar()
+        Borrar();
     });
 
     introValor()
 }
 function introValor() {
+
     document.getElementById("searchTerm").addEventListener("keypress", (e) => {
+        $("#table").remove();
+//        document.getElementById("table").innerHTML = "";
+        (document.getElementById('resulDescripcion')).innerHTML = "";
         if (e.key !== "Enter") {
             let searchTerm = (<HTMLInputElement>document.getElementById('searchTerm')).value + e.key;
             searchTerm=searchTerm.toLocaleLowerCase() 
@@ -23,41 +35,36 @@ function introValor() {
             var encontrado = ddbb.filter(function (element) {
 
                 return element.articulo.toLocaleLowerCase().includes(searchTerm) ||
-                    element.codigo2.toLocaleLowerCase().includes(searchTerm) ||
-                    element.codprov.toLocaleLowerCase().includes(searchTerm) ||
-                    element.descripcion.toLocaleLowerCase().includes(searchTerm);
+                       element.codigo2.toLocaleLowerCase().includes(searchTerm) ||
+                       element.codprov.toLocaleLowerCase().includes(searchTerm) ||
+                       element.descripcion.toLocaleLowerCase().includes(searchTerm);
             })
             const table=document.createElement("table")
+  
             console.log(encontrado)
             encontrado.forEach(element=>{
                 const node=document.createElement("tr");
                 const td1=document.createElement("td");
-                td1.innerHTML=element.articulo;
-                node.appendChild(td1);
+                    td1.innerHTML=element.articulo;
+                    node.appendChild(td1);
                 const td2=document.createElement("td");
-                td2.innerHTML=element.descripcion;
-                node.appendChild(td2);
-                const td3=document.createElement("td");
-                td3.innerHTML=element.precio;
-                node.appendChild(td3);
-                table.appendChild(node);
+                    td2.innerHTML=element.descripcion;
+                    node.appendChild(td2);
+                    const td3=document.createElement("td");
+                    td3.innerHTML=element.precio;
+                    node.appendChild(td3);
+                    table.appendChild(node);
+                    const td4=document.createElement("td");
+                    td3.innerHTML=element.stock;
+                    node.appendChild(td4);
+                    table.appendChild(node);
             })                
             document.getElementById('resulDescripcion').appendChild(table);                   
-
-
-//            (document.getElementById('resulDescripcion')).innerHTML =
-//                encontrado.descripcion + " ====>" + encontrado.precio;
-
-
-
-            //                encontrado.includes(searchTerm);
-            //                console.log(encontrado);
-
-        }
+         }
     })
 }
 function Borrar() {
-
+    
     (<HTMLInputElement>document.getElementById('searchTerm')).value = "";
     document.getElementById('searchTerm').focus();
     //@ts-ignore
