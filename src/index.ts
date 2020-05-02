000e2053fea228dd557c48b16007c9916175fd7e
shiverazul <shiverazul@gmail.com>
2020-05-02 21:16:40 +0200

    Beta1 funcionando

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-
+import { Elemento } from './class.element';
 const $ = require("jquery");
 
-let ddbb = null;
+let ddbb: Array<Elemento> = [];
 
-$.getJSON("A-HTML.json", function (JSON) {
-    ddbb = JSON;
+$(document).ready(function () {
+    document.getElementById("table3").innerHTML = "<tr><th>Descricion</th><th>Precio</th><th>Cant</th><th>Desc</th><th>TOTAL</th></tr>";
+
+    $.getJSON("A-HTML.json", function (data) {
+        ddbb = data.map(element => new Elemento(element.articulo, element.codigo2, element.codprov, element.descripcion, element.precio, element.stock));
+
+        init();
+    });
 });
-
-document.getElementById("table3").innerHTML= "<tr><th>Descricion</th><th>Precio</th><th>Cant</th><th>Desc</th><th>TOTAL</th></tr>";
-
-
-
-init();
 
 function init() {
     document.getElementById("borrar").addEventListener("click", () => {
@@ -22,57 +22,53 @@ function init() {
 }
 
 function introValor() {
+    $("#searchTerm").on("input", function () {
 
-    $(document).ready(function () {
+        $("#table").remove();
+        (document.getElementById('resulDescripcion')).innerHTML = "";
+        let idx = 1;
+        let searchTerm = ($(this).val());
 
-        $("#searchTerm").on("input", function () {
+        searchTerm = searchTerm.toLocaleLowerCase();
 
-            $("#table").remove();
-            (document.getElementById('resulDescripcion')).innerHTML = "";
-            let idx = 1;
-            let searchTerm = ($(this).val());
+        var encontrado = ddbb.filter(function (element) {
+            if (isNaN(searchTerm)) {  //si es numero o no 
+                return element.descripcion.toLocaleLowerCase().includes(searchTerm);
+            }
+            else {
+                return element.articulo.toLocaleLowerCase().includes(searchTerm) ||
+                    element.codigo2.toLocaleLowerCase().includes(searchTerm) ||
+                    element.codprov.toLocaleLowerCase().includes(searchTerm);
+            }
+        })
 
-            searchTerm = searchTerm.toLocaleLowerCase();
+        const table = document.createElement("table")
+        encontrado.forEach(element => {
+            const node = document.createElement("tr");
+            node.addEventListener("click", () => {
 
-            var encontrado = ddbb.filter(function (element) {
-                if (isNaN(searchTerm)) {  //si es numero o no 
-                    return element.descripcion.toLocaleLowerCase().includes(searchTerm);
-                }
-                else {
-                    return element.articulo.toLocaleLowerCase().includes(searchTerm) ||
-                        element.codigo2.toLocaleLowerCase().includes(searchTerm) ||
-                        element.codprov.toLocaleLowerCase().includes(searchTerm) ;
-                } 
-            })
+                tabla2(element);
+            });
 
-            const table = document.createElement("table")
-            encontrado.forEach(element => {
-                const node = document.createElement("tr");
-                node.addEventListener("click", () => {
+            if (idx <= 80) {
 
-                    tabla2(element);
-                });
+                const td2 = document.createElement("td");
+                td2.innerHTML = element.descripcion;
+                node.appendChild(td2);
+                table.appendChild(node);
+                const td3 = document.createElement("td");
+                td3.innerHTML = element.precio + "";
+                node.appendChild(td3);
+                table.appendChild(node);
+                const td4 = document.createElement("td");
+                td4.setAttribute('style', 'color: green');
+                td4.innerHTML = element.stock + "";
+                node.appendChild(td4);
+                table.appendChild(node);
+                document.getElementById('resulDescripcion').appendChild(table);
+                ++idx;
 
-                if (idx <= 80) {
-
-                    const td2 = document.createElement("td");
-                    td2.innerHTML = element.descripcion;
-                    node.appendChild(td2);
-                    table.appendChild(node);
-                    const td3 = document.createElement("td");
-                    td3.innerHTML = element.precio;
-                    node.appendChild(td3);
-                    table.appendChild(node);
-                    const td4 = document.createElement("td");
-                    td4.setAttribute('style', 'color: green');
-                    td4.innerHTML = element.stock;
-                    node.appendChild(td4);
-                    table.appendChild(node);
-                    document.getElementById('resulDescripcion').appendChild(table);
-                    ++idx;
-
-                }
-            })
+            }
         })
     })
 }
@@ -86,35 +82,84 @@ function Borrar() {
 }
 
 function tabla2(element) {
+    //Creacion de TH
+
     const table2 = document.getElementById("table3");
-        const node = document.createElement("tr");
+    const node = document.createElement("tr");
 
-        const td1 = document.createElement("td");
-            td1.innerHTML = element.descripcion;
-            node.appendChild(td1);
-            table2.appendChild(node);
-        const td2 = document.createElement("td");
-            td2.innerHTML = element.precio;
-            node.appendChild(td2);
-            table2.appendChild(node);
-        const td3 = document.createElement("td");
+    const td1 = document.createElement("td");
+    td1.innerHTML = element.descripcion;
+    node.appendChild(td1);
+    table2.appendChild(node);
+    const td2 = document.createElement("td");
+    td2.innerHTML = element.precio;
+    node.appendChild(td2);
+     table2.appendChild(node);
+    const td3 = document.createElement("td");
+    const newcantidad = document.createElement("input");
+    newcantidad.value = "1";
+    newcantidad.className = "noinput";
+    //           node.addEventListener("click", () => {
+    //            alert(element);
+    //        });
+    td3.appendChild(newcantidad); //introduzco input dentro de td3
+    node.appendChild(td3);
+    table2.appendChild(node);
 
-        node.addEventListener("click", () => {
-            alert(element);
-        });
 
-            td3.innerHTML = "1";
-            node.appendChild(td3);
-            table2.appendChild(node);
-        const td4 = document.createElement("td");
-            td4.innerHTML = "";
-            node.appendChild(td4);
-            table2.appendChild(node);
-        const td5 = document.createElement("td");
-            td5.innerHTML = element.precio;
-            node.appendChild(td5);
-            table2.appendChild(node);
-}       
+    const td4 = document.createElement("td");
+
+    const newdescuento = document.createElement("input");
+    newdescuento.value = "0";
+    newdescuento.className = "noinput";
+    td4.appendChild(newdescuento); //introduzco input dentro de td4
+    node.appendChild(td4);
+    table2.appendChild(node);
+
+    const td5 = document.createElement("td");
+    const newtotal = document.createElement("input");
+    newtotal.value = element.precio;
+
+    newtotal.className = "noinput total";
+    td5.appendChild(newtotal); //introduzco input dentro de td5
+
+    node.appendChild(td5);
+    table2.appendChild(node);
+    getTotal()
+    $(newcantidad).on("input", function () {
+        anadirPreciodeElemento(element.precio, +newcantidad.value, +newdescuento.value, newtotal);
+        getTotal();
+    });
+    $(newdescuento).on("input", function () {
+        anadirPreciodeElemento(element.precio, +newcantidad.value, +newdescuento.value, newtotal);
+        getTotal();
+    });
+    Borrar();
+
+}
+
+function anadirPreciodeElemento(precio: number, cantidad: number, newdescuento: number, totalElemento: HTMLElement): void {
+    //console.log(totalElemento, precio, cantidad);
+    const desc: number = precio*(newdescuento / 100);
+    //console.log(newdescuento)
+    const total: number = (precio-desc )* cantidad;
+    (<HTMLInputElement>totalElemento).value = total.toFixed(2);
+}
+
+function getTotal() {
+    const elements = $("#table3").find(".total");
+    let total: number = 0;
+    for (let i = 0; i < elements.length; i++) {
+//        console.log(elements[i].value)
+        total += +elements[i].value;
+        document.getElementById('resulTotalisimo').innerHTML = total.toFixed(2)
+
+    }
+    
+    //console.log(total);
+}
+
+
 
 //FILTRO
 //function doSearch() {
@@ -144,4 +189,3 @@ function tabla2(element) {
 	//FOREACH
 //	arr.forEach( (elem) => console.log(elem));
 //	 
-
