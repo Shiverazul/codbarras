596935b9a7260eca2629027f0dffa0ddfd664eac
shiverazul <shiverazul@gmail.com>
2020-05-06 23:40:16 +0200

    screenshots

---
+++ src/class.element.ts
@@ -0,0 +1,18 @@
+
+export class Elemento {
+    public articulo: string;
+    public codigo2: string;
+    public codprov: string;
+    public descripcion: string;
+    public precio: number;
+    public stock: number;
+
+    constructor(articulo: string, codigo2: string, codprov: string, descripcion: string, precio: string, stock: string) {
+        this.articulo = articulo.replace(/\s/g, '');
+        this.codigo2 = codigo2.replace(/\s/g, '');
+        this.codprov = codprov.replace(/\s/g, '');
+        this.descripcion = descripcion;
+        this.precio = +precio.replace(/\s/g, '').replace(',', '.');
+        this.stock = +stock.replace(/\s/g, '').replace(',', '.');
+    }
+}
