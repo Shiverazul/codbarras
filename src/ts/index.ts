dc3ca2811be0608021822f7c1c43d6dd990d5792
shiverazul <shiverazul@gmail.com>
2020-07-14 15:59:43 +0200

    Fecha Base

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -61,10 +61,14 @@ function introValor() {
  
         var encontrado = ddbb.filter(function (element) {
             // fecha de la base
-            //if (element.descripcion.toLocaleLowerCase().includes("?date")) { // fecha de la base
-            //    (document.getElementById('verBase')).innerHTML = element.descripcion.substring(0, 8);
-            //};
+           if ((<HTMLInputElement>document.getElementById('verBase')).value = "Fecha Base") { 
+                if (element.descripcion.toLocaleLowerCase().includes("?date")) { // fecha de la base
+                    (document.getElementById('verBase')).innerHTML = element.descripcion.substring(0, 8);
+          }
+            };
 
+
+            
             if (isNaN(searchTerm)) {  //si es numero o no 
                 return element.descripcion.toLocaleLowerCase().includes(searchTerm);
             }
